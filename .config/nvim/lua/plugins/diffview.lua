42d797d8b55cdf27ae30dc8d5e90ad84dedc3244
stolmen <edward93@gmail.com>
2024-11-20 15:35:05 +1100

    add new keymap for invoking diffview comparing the current state of the working tree against `master`. update .gitconfig to automatically rebase on    pull if the local tree has diverged from remote.

--- .config/nvim/lua/plugins/diffview.lua
+++ .config/nvim/lua/plugins/diffview.lua
@@ -5,16 +5,7 @@ return {
     config = function()
       vim.api.nvim_set_keymap("n", "<leader>gd", "<cmd>DiffviewOpen<CR>", { noremap = true, silent = true })
       vim.api.nvim_set_keymap("n", "<leader>gD", "<cmd>DiffviewClose<CR>", { noremap = true, silent = true })
+      vim.api.nvim_set_keymap("n", "<leader>gm", "<cmd>DiffviewOpen master<CR>", { noremap = true, silent = true })
     end,
-    -- opts = {
-    --   -- transparent_mode = true,
-    -- },
   },
-  -- -- Configure LazyVim to load gruvbox
-  -- {
-  --   "LazyVim/LazyVim",
-  --   opts = {
-  --     colorscheme = "gruvbox",
-  --   },
-  -- },
 }
